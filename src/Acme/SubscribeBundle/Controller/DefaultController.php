0a6b1c7dd689758b603111b3f9fc143041fcc6d8
kokcito13 <oklosovich@gmail.com>
2015-01-30 17:03:20 +0200

    f

--- src/Acme/SubscribeBundle/Controller/DefaultController.php
+++ src/Acme/SubscribeBundle/Controller/DefaultController.php
@@ -28,15 +28,18 @@ class DefaultController extends Controller
             $email = trim($email);
             if (!empty($email) || filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 $em = $this->getDoctrine()->getManager();
+                /** @var Subscriber|null $entity */
                 $entity = $em->getRepository('AcmeSubscribeBundle:Subscriber')->findOneByEmail($email);
                 if (!$entity) {
                     $entity = new Subscriber();
                     $entity->setName($email);
                     $entity->setEmail($email);
                     $entity->setStatus(Subscriber::STATUS_ACTIVE);
-                    $em->persist($entity);
-                    $em->flush();
+                } else {
+                    $entity->setStatus(Subscriber::STATUS_ACTIVE);
                 }
+                $em->persist($entity);
+                $em->flush();
                 $arr['success'] = true;
             } else {
                 $arr['error'] = 'Email is empty';
@@ -46,5 +49,43 @@ class DefaultController extends Controller
         }
 
         return new JsonResponse($arr);
+    }
+
+    /**
+     * @Route("/un/{hash}", name="un_subscriber")
+     * @Template()
+     * @Method("GET")
+     */
+    public function unAction(Request $request, $hash = '')
+    {
+        $email = '';
+        $hash = trim($hash);
+        if (empty($hash)) {
+            throw $this->createNotFoundException('Просим нас извинить но данную страницу мы неможем найти.');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $sql = "SELECT id FROM subscribers WHERE MD5(email) = '".$hash."'";
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        if (!empty($result) && isset($result[0]) && isset($result[0]['id'])) {
+            $id = (int)$result[0]['id'];
+            $entity = $em->getRepository('AcmeSubscribeBundle:Subscriber')->find($id);
+            $entity->setStatus(Subscriber::STATUS_DELETE);
+
+            $em->persist($entity);
+            $em->flush();
+
+            $email = $entity->getEmail();
+        } else {
+            throw $this->createNotFoundException('Просим нас извинить но данную страницу мы неможем найти.');
+        }
+
+        return array(
+            'email' => $email
+        );
     }
 }
