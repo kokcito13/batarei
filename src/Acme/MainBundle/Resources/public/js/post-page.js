be45b5402612fa3063bba93208e168e7a32a1cfa
kokcito13 <oklosovich@gmail.com>
2015-07-30 16:25:49 +0300

    fixing

--- src/Acme/MainBundle/Resources/public/js/post-page.js
+++ src/Acme/MainBundle/Resources/public/js/post-page.js
@@ -8,19 +8,13 @@ $(function(){
                     if (data.success) {
                         if (data.data.shortText && data.data.shortText.length > 100) {
                             $('#offerShortText').html(data.data.shortText);
-
-                            $('div#offerShortText > ul').addClass('list mark-circle');
-                            $('div#offerShortText > ol').addClass('list mark-circle-numbers');
                         }
                         if (data.data.text && data.data.text.length > 100) {
                             $('#offerText').html(data.data.text);
-
-                            $('div#offerText > ul').addClass('list mark-circle');
-                            $('div#offerText > ol').addClass('list mark-circle-numbers');
                         }
                     } else
                         console.log(data);
                 });
         }
-    }, 1500);
+    }, 2500);
 });
